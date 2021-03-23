49feca884892609d96e3934c6f386c93f65be1c6
SerjQazi <sirajum.qazi@gmail.com>
2021-03-23 08:31:35 -0400

    added animated project thumbnail, set thumbnail onChange darkmode

--- src/script.js
+++ src/script.js
@@ -5,4 +5,10 @@ checkbox.addEventListener('change', () => {
   const bodyElement = document.querySelector('body');
   bodyElement.classList.toggle('bodyDark');
 
+  const fmbLight = document.querySelector('video.fmbLight');
+  fmbLight.classList.toggle('displayOff');
+  
+  const fmbDark = document.querySelector('video.fmbDark');
+  fmbDark.classList.toggle('displayOn');
+
 });
