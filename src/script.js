1e3a2c8c2c04e5dca477949231e98d9afa49e1dc
SerjQazi <sirajum.qazi@gmail.com>
2021-03-28 10:04:09 -0400

    adjusted colors for dark mode

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-import {rtContent, fmbContent, rlContent, shContent} from './modalContent.js'
+import { rtContent, fmbContent, rlContent, shContent } from "./modalContent.js";
 
 // flickity carousel options
 const element = document.querySelector(".main-carousel");
@@ -20,7 +20,6 @@ const fmbModal = new tingle.modal(modalOptions);
 const rlModal = new tingle.modal(modalOptions);
 const shModal = new tingle.modal(modalOptions);
 
-
 rtModal.setContent(rtContent);
 const roboTrivia = document.querySelector(".rtContainer");
 roboTrivia.addEventListener("click", () => rtModal.open());
@@ -36,8 +35,6 @@ recipeLookup.addEventListener("click", () => rlModal.open());
 shModal.setContent(shContent);
 const sanctuaryHelper = document.querySelector(".shContainer");
 sanctuaryHelper.addEventListener("click", () => shModal.open());
-
-
 
 // darkMode toggle function
 const checkbox = document.getElementById("checkbox");
@@ -62,8 +59,26 @@ checkbox.onchange = () => {
   const h1Element = document.querySelector("h1");
   h1Element.classList.toggle("h1Dark");
 
+  const socialIcons = document.querySelectorAll(".socialIcons");
+  socialIcons.forEach((socialIconEl) => {
+    socialIconEl.classList.toggle("darkSocial");
+  });
+
   const h3Element = document.querySelectorAll("h3");
   h3Element.forEach((h3) => {
     h3.classList.toggle("h3Dark");
   });
+
+  const nextButton = document.querySelectorAll(".flickity-prev-next-button");
+  nextButton.forEach((nextBtn) => {
+    nextBtn.classList.toggle("darkButtons");
+  });
+
+  const inputElement = document.querySelectorAll("input");
+  inputElement.forEach((input) => {
+    input.classList.toggle("darkInput");
+  });
+
+  const textareaEl = document.querySelector("textarea");
+  textareaEl.classList.toggle("darkTextarea");
 };
